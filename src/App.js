158acd56f39ef5ea486497372a794d45ced33dbe
Carlos Baraza Haro <carlos.baraza@ieee.org>
2018-01-27 18:41:31 +0000

    Add jokes dataset and sort descending by createdAt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import jokes from "./jokes.json";
+
+const sortedJokes = jokes.sort(
+  (j1, j2) => (j1.createdAt < j2.createdAt ? 1 : -1)
+);
+
+const joke = sortedJokes[0];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Programmers count from 0...</h2>
+        <h2
+          dangerouslySetInnerHTML={{
+            __html: joke.body
+          }}
+        />
       </div>
     );
   }
