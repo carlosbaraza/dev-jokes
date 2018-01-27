8e511a2f4cf6ec51f38e4c821c01a0bd32a02b53
Carlos Baraza Haro <carlos.baraza@ieee.org>
2018-01-27 17:22:01 +0000

    Add enzyme-to-json serializer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 // setup file
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import enzymeSerializer from "enzyme-to-json/serializer";
 
 configure({ adapter: new Adapter() });
+
+expect.addSnapshotSerializer(enzymeSerializer);
